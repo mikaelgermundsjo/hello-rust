d2f1c04640ec2c4a12405235ffcccd43084b678c
Mikael Germundsjo <mikael@stalgarden.se>
2022-04-18 14:46:39 +0200

    new hello message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, Mikael!");
+    let stdout = stdout();
+    let s = "Hello fellow Rustaceans!";
+    let message = String::from(s);
+    let chars = message.chars();
+    let width = chars.count();
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
